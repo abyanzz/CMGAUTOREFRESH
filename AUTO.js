39ef4448524717ffdba50d2cc9c51d26758a2d2a
abyanzz <jilclix7@gmail.com>
2022-12-30 17:53:41 +0700

    Create AUTO.js

---
+++ AUTO.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         CRPTOMININGGAME - REFILL
+// @namespace    http://cryptomininggame.com/
+// @version      0.2
+// @description  auto fill
+// @author       zenix
+// @match        https://cryptomininggame.com/*
+// @icon         https://cryptomininggame.com/favicons/apple-icon-57x57.png
+// @grant        none
+// ==/UserScript==
+setTimeout(function(){ location.reload(); }, 1*1000);
+(function() {
+document.getElementsByClassName("btnEnergy disabled animate__animated animate__flash")[0].click();
+})();
+setTimeout(function(){ location.reload(); }, 1*1000);
